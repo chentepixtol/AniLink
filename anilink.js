f0a95753f9ebe5316a3c3f6ca4da3c9f85a4470b
Chentepixtol <chentepixtol>
2010-01-17 15:08:58 -0600

    agregada la clase de anilink

---
+++ anilink.js
@@ -0,0 +1,32 @@
+ var Anilink = new Class({
+     Implements: [ Options ],
+     options: {
+       enterClass: 'link-enter',
+       leaveClass: 'link-leave',
+	   morph: {
+	   	 duration: 500,
+		 transition: 'quad:out',
+		 onComplete: $empty
+	   }      
+     },
+     Links: [],
+     initialize: function(elements, options){
+       this.setOptions(options);
+       Links = $$(elements);
+       Links.each(function(link, i){
+	   
+          link.addEvent('mouseenter', function(){
+            link.removeClass(this.options.leaveClass);
+			link.set('morph', this.options.morph);
+			link.morph('.' + this.options.enterClass );
+          }.bind(this));
+
+          link.addEvent('mouseleave', function(){
+            link.removeClass(this.options.enterClass);
+			link.set('morph',$merge(this.options.morph ,{onComplete: function(){link.setStyle('background-color','transparent')} }));
+            link.morph('.' + this.options.leaveClass);
+          }.bind(this));       
+
+       }, this);
+     }
+  });
