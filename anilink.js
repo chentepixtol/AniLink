24eb24551ec5024b33280bbfde7dd4ff3f98a7bd
Chentepixtol <chentepixtol>
2010-01-24 19:43:31 -0600

    Espacios

--- anilink.js
+++ anilink.js
@@ -1,34 +1,36 @@
- var Anilink = new Class({
-     Implements: [ Options ],
-     options: {
-       enterClass: 'link-enter',
-       leaveClass: 'link-leave',
-       morph: {
-	   	 duration: 500,
-		 transition: 'quad:in:out',
-		 onComplete: $empty
-	   }      
-     },
-     Links: [],
-     initialize: function(elements, options){
-       this.setOptions(options);
-       Links = $$(elements);
-       Links.each(function(link, i){
-	   
-          link.addEvent('mouseenter', function(){
-            link.removeClass(this.options.leaveClass);
-			link.set('morph', this.options.morph);
-			link.morph('.' + this.options.enterClass );
-          }.bind(this));
+var Anilink = new Class( {
+	Implements : [ Options ],
+	options : {
+		enterClass : 'link-enter',
+		leaveClass : 'link-leave',
+		morph : {
+			duration : 500,
+			transition : 'quad:in:out',
+			onComplete : $empty
+		}
+	},
+	Links : [],
+	initialize : function(elements, options) {
+		this.setOptions(options);
+		Links = $$(elements);
+		Links.each(function(link, i) {
 
-          link.addEvent('mouseleave', function(){
-            link.removeClass(this.options.enterClass);
-			link.set('morph', $merge( this.options.morph ,{
-                onComplete: function(){ link.setStyle('background-color','transparent') }})
-            );
-            link.morph('.' + this.options.leaveClass);
-          }.bind(this));       
+			link.addEvent('mouseenter', function() {
+				link.removeClass(this.options.leaveClass);
+				link.set('morph', this.options.morph);
+				link.morph('.' + this.options.enterClass);
+			}.bind(this));
 
-       }, this);
-     }
-  });
+			link.addEvent('mouseleave', function() {
+				link.removeClass(this.options.enterClass);
+				link.set('morph', $merge(this.options.morph, {
+					onComplete : function() {
+						link.setStyle('background-color', 'transparent');
+					}
+				}));
+				link.morph('.' + this.options.leaveClass);
+			}.bind(this));
+
+		}, this);
+	}
+});
